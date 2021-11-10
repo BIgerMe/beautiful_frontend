d8e4990ba625f68bb0e5521e01dd12972d2fdf92
zxm <xiao3017@sina.com>
2021-11-10 17:28:01 +0800

    '1'

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,20 +1,24 @@
 <template>
   <el-row :gutter="20">
-    <el-col :xs="24" :sm="24"  :md="4" :lg="4" :xl="4">
-      <el-card shadow="category">
-        <a-menu
-          style="width: auto"
-          :default-selected-keys="['php']"
-          mode="inline"
-          theme="light"
-        >
-          <a-menu-item key="php">
-            php
-          </a-menu-item>
-          <a-menu-item key="python">
-            python
-          </a-menu-item>
-        </a-menu>
+    <el-col :xs="24" :sm="24"  :md="4" :lg="4" :xl="4" style="max-height:80vh">
+      <el-scrollbar wrap-class="categoryList" wrap-style="color: red;" view-style="font-weight: bold;" view-class="view-box" :native="false">
+        <el-card shadow="always">
+          <a-menu
+            style="width: auto"
+            :default-selected-keys="['php']"
+            mode="inline"
+            theme="light"
+          >
+            <a-menu-item v-for="(item, index) in category" :key="index">
+              {{index}}
+            </a-menu-item>
+          </a-menu>
+        </el-card>
+      </el-scrollbar>
+    </el-col>
+    <el-col :xs="24" :sm="24"  :md="14" :lg="14" :xl="14">
+      <el-card shadow="always">
+        总是显示
       </el-card>
     </el-col>
     <el-col :xs="24" :sm="24"  :md="6" :lg="6" :xl="6">
@@ -22,26 +26,41 @@
         总是显示
       </el-card>
     </el-col>
-    <el-col :xs="24" :sm="24"  :md="14" :lg="14" :xl="14">
-      <el-card shadow="always">
-        总是显示
-      </el-card>
-    </el-col>
   </el-row>
 </template>
 <script>
+  import { categoryCY } from '@/api/blog'
+  import * as echarts from "echarts";
   export default {
-    name: 'Test',
+    name: 'blog',
     data() {
-      return { show: true }
+      return {
+        category:[]
+      }
     },
-    created() {},
+    created() {
+      this.fetchData()
+    },
     mounted() {},
-    methods: {},
+    methods: {
+      async fetchData(){
+        const { data } = await categoryCY()
+        this.category = data
+      },
+      overCategory(){
+        console.log('123')
+      },
+      leaveCategory(){
+        console.log('321')
+      }
+    },
   }
 </script>
 <style>
-  .ant-menu-vertical{
-    border-right: 0 !important;
+  .ant-menu-inline, .ant-menu-vertical, .ant-menu-vertical-left {
+    border-right: 0 solid #e8e8e8!important;
+  }
+  .categoryList {
+    max-height: 80vh;
   }
 </style>
