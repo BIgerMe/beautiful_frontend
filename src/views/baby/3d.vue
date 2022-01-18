86315119e1ad685d9d6775d102e6a7090bf2c133
zxm <xiao3017@sina.com>
2022-01-18 18:21:26 +0800

    '3d'

--- src/views/baby/3d.vue
+++ src/views/baby/3d.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="phoenix" class="index-container" style="width: 100%;height: 400px"></div>
+  <div id="phoenix" class="index-container" style="width: 100%;height: 800px"></div>
 </template>
 
 <script>
@@ -17,10 +17,88 @@
     },
     created() {},
     mounted() {
-      // this.horse()
-      this.phoenix()
+      this.space()
+      // this.phoenix()
     },
     methods: {
+      space(){
+        let container, stats
+        let camera, scene, renderer
+        let mesh
+        let controls
+        const radius = 2000
+        function init() {
+          container = document.getElementById('phoenix')
+          camera = new THREE.PerspectiveCamera(
+            45,
+            container.offsetWidth / container.offsetHeight,
+            1,
+            10000
+          )
+          camera.position.set(0,500,0)
+
+          scene = new THREE.Scene()
+          /*从上往下*/
+          // const light1 = new THREE.DirectionalLight(0xFFFFFF, 4)
+          // light1.position.set(1, 1, 0).normalize()
+          // scene.add(light1)
+
+          // const light = new THREE.AmbientLight( 0xf00 ); // soft white light
+          // scene.add( light );
+          // const light1 = new THREE.AmbientLight( 0x0f0 ); // soft white light
+          // scene.add( light1 );
+
+          // const light2 = new THREE.DirectionalLight(0xFFFFFF, 3)
+          // light2.position.set(1, -1, 0).normalize()
+          // scene.add(light2)
+          const loader = new THREE.GLTFLoader()
+          // loader.load('https://video.xxroom.xyz/scene.glb', function (gltf) {
+          loader.load('./static/three/gltf/need_some_space/scene.gltf', function (gltf) {
+          // loader.load('./static/three/gltf/nss.glb', function (gltf) {
+
+            let mesh = gltf.scene
+            mesh.position.set(-100,0,100)
+            scene.add(mesh)
+            // scene.add( mesh1,mesh2 );
+            // mesh.push(mesh1,mesh2);
+            // mixer.push(  mixer1,mixer2 );
+            // scene.add(mesh1)
+            // mixer = new THREE.AnimationMixer(mesh1)
+            // mixer.clipAction(gltf.animations[0]).setDuration(1).play()
+          })
+
+          renderer = new THREE.WebGLRenderer( {alpha: true } );
+          //您可以将透明颜色保留为默认值。
+          renderer.setClearColor( 0x000000); //default
+          renderer.setPixelRatio(window.devicePixelRatio) //像素比
+          renderer.setSize(container.offsetWidth, container.offsetHeight)
+          // renderer.outputEncoding = THREE.sRGBEncoding //真彩色，不加的话颜色会与ps中图像看上去的不同
+          container.appendChild(renderer.domElement)
+
+          controls = new THREE.OrbitControls(camera,renderer.domElement);// 初始化控制器
+          // controls.target.set(0, 0, 0);// 设置控制器的焦点，使控制器围绕这个焦点进行旋转
+          // controls.maxPolarAngle = Math.PI / 3;//绕垂直轨道的距离（范围是0-Math.PI,默认为Math.PI）
+          // controls.autoRotate = false;
+        }
+        function animate() {
+          requestAnimationFrame(animate)
+          render()
+        }
+        function render() {
+          /*相机的位置*/
+          // theta += 0.2
+          /*头正尾负*/
+          // camera.position.x = radius * Math.sin(THREE.MathUtils.degToRad(theta))
+          // camera.position.y = radius * Math.cos(THREE.MathUtils.degToRad(theta))
+          // camera.position.z = radius * Math.cos(THREE.MathUtils.degToRad(theta))
+          /*相机看向的位置*/
+          // camera.lookAt(0, 0, 0)
+
+          renderer.render(scene, camera)
+        }
+        init()
+        animate()
+      },
       phoenix() {
         let container, stats
         let camera, scene, renderer
