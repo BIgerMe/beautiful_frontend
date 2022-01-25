d0a1fffe2a9edc715c743860b030e5dfd2aec764
xiaoming <xiao3017@sina.com>
2022-01-25 22:51:48 +0800

    '1'

--- src/views/three/two.vue
+++ src/views/three/two.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+  import { clone } from '@/utils/three'
   export default {
     name: 'two',
     components: {},
@@ -27,12 +28,16 @@
           let width = container.offsetWidth <= 414 ? window.innerWidth :  container.offsetWidth
           let height = container.offsetHeight <= 414 ? window.innerHeight : container.offsetHeight
           camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000)
-          camera.position.set(0,50,0)
+          camera.position.set(0,3500,0)
           scene = new THREE.Scene()
           /*从上往下*/
           const light1 = new THREE.DirectionalLight(0xFFFFFF, 1)
           light1.position.set(0,1,0).normalize()
           scene.add(light1)
+          const light2 = new THREE.DirectionalLight(0xFFFFFF, 1)
+          light1.position.set(0,-1,0).normalize()
+          scene.add(light2)
+
           const gt = new THREE.TextureLoader().load( "./static/three/grasslight-big.jpg" );
           // const gt = new THREE.TextureLoader().load( "http://head.xxroom.xyz/lisgsx4cwBF8-6HnXbsP51pob76_" );
           const gg = new THREE.PlaneGeometry( 192, 108,1,1 );
@@ -50,84 +55,41 @@
           const loader = new THREE.GLTFLoader()
           loader.load('./static/three/gltf/two.glb', function (gltf) {
             mesh[0] = gltf.scene
-            scene.add(mesh[0])
-            console.log(gltf)
-            // mesh[0].scale.set(3,3,3)
-            // mesh[0].position.y = 4
-            // mesh[0].position.z = 14
-            // mesh[0].position.x = 12
-            // console.log(gltf.scene)
             mesh[0].traverse((child) => {
               // 设置线框材质
-              // console.log(child)
               // console.log(child.isMesh)
               if (child.isMesh) {
+                // console.log(child)
+                // setCityLineMaterial(child)
                 if (child.name.indexOf('Car') !== -1) {
-                  // console.log(child)
-                  //   const material = new THREE.MeshBasicMaterial({
-                  //     color: "rgb(35,35,40)",
-                  //   });
-                  child.material.color.r = 0.1
-                  child.material.color.g = 0.1
-                  child.material.color.b = 0.1
+                    // const material = new THREE.MeshBasicMaterial({
+                    //   color: "rgb(238,232,232)",
+                    // });
+                  // child.material.color.r = 0.1
+                  // child.material.color.g = 0.1
+                  // child.material.color.b = 0.1
                   // console.log(child.material.color)
 
-                    // const mesh = new THREE.Mesh(child.geometry, material);
-                    // mesh.position.set(child.position.x, child.position.y, child.position.z);
-                    // scene.add(mesh)
+                  // const mesh = new THREE.Mesh(child.geometry, child.material);
+                  // mesh.position.set(child.position.x, child.position.y, child.position.z);
+                  // // mesh.rotation = child.rotation
+                  // mesh.rotateX(child.rotation._x);
+                  // mesh.rotateY(child.rotation._y);
+                  // mesh.rotateZ(child.rotation._z);
+                  const mesh = clone(child)
+                  scene.add(mesh)
+                }else{
+                  const mesh = new THREE.Mesh(child.geometry, child.material);
+                  mesh.position.set(child.position.x, child.position.y, child.position.z);
+                  // mesh.rotation = child.rotation
+                  mesh.rotateX(child.rotation._x);
+                  mesh.rotateY(child.rotation._y);
+                  mesh.rotateZ(child.rotation._z);
+
+                  scene.add(mesh)
                 }
               }
-              //   const material = new THREE.MeshBasicMaterial({
-              //     color: "rgb(27,168,47)",
-              //   });
-              //   const mesh = new THREE.Mesh(child.geometry, material);
-              //   mesh.position.set(
-              //     child.position.x,
-              //     child.position.y,
-              //     child.position.z
-              //   );
-              //   scene.add(mesh)
-              // }
-              // if (child.isMesh) {
-              //   //这个判断模型是楼房还是其他  加载不同的材质
-              //   if (["CITY_UNTRIANGULATED"].includes(child.name)) {
-              //     // 拿到模型线框的Geometry
-              //     this.setCityLineMaterial(child);
-              //     this.setCityMaterial(child);
-              //   } else if (["ROADS"].includes(child.name)) {
-              //     //道路
-              //     const material = new THREE.MeshBasicMaterial({
-              //       color: "rgb(41,46,76)",
-              //     });
-              //     const mesh = new THREE.Mesh(child.geometry, material);
-              //     mesh.rotateX(-Math.PI / 2);
-              //     mesh.position.set(
-              //       child.position.x,
-              //       child.position.y,
-              //       child.position.z
-              //     );
-              //     scene.add(mesh);
-              //   } else {
-              //     //地面
-              //     const material = new THREE.MeshBasicMaterial({
-              //       color: "#040912",
-              //     });
-              //     const mesh = new THREE.Mesh(child.geometry, material);
-              //     scene.add(mesh);
-              //     mesh.rotateX(-Math.PI / 2);
-              //     mesh.position.set(
-              //       child.position.x,
-              //       child.position.y,
-              //       child.position.z
-              //     );
-              //   }
-              // }
-              // 设置线框材质
             });
-            // mesh[0] = gltf.scene
-            // mesh[0].scale.set(3,3,3)
-            // mesh[0].position.y= 4
-            // scene.add(mesh[0])
           })
           const axesHelper = new THREE.AxesHelper( 50 );
           scene.add( axesHelper );
@@ -135,7 +97,7 @@
           renderer = new THREE.WebGLRenderer( {alpha: true,antialias:true, } );
           // renderer = new THREE.WebGLRenderer();
           //您可以将透明颜色保留为默认值。
-          renderer.setClearColor( 0x000); //default
+          renderer.setClearColor( 0x000,0); //default
           renderer.setPixelRatio(window.devicePixelRatio) //像素比
           renderer.setSize(width, height)
           renderer.outputEncoding = THREE.sRGBEncoding //真彩色，不加的话颜色会与ps中图像看上去的不同
@@ -146,7 +108,26 @@
 
           // renderer.autoClear = false
         }
+        function setCityLineMaterial(object) {
+          const edges = new THREE.EdgesGeometry(object.geometry, 1);
+          //设置模型的材质
+          const lineMaterial = new THREE.LineBasicMaterial({
+            // 线的颜色
+            color: "rgb(254,38,88)",
+          });
+          //把数据组合起来
+          const lineS = new THREE.LineSegments(edges, lineMaterial);
+          //设置数据的位置
+          lineS.position.set(
+            object.position.x,
+            object.position.y,
+            object.position.z
+          );
+          //添加到场景
+          scene.add(lineS);
 
+          // lineS.rotateX(-Math.PI / 2);
+        }
         function animate() {
           requestAnimationFrame(animate)
           render()
