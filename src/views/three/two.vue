2d493aefb86f664b87126ec8593fdf7b0d2a7660
xiaoming <xiao3017@sina.com>
2022-01-26 21:19:47 +0800

    '1'

--- src/views/three/two.vue
+++ src/views/three/two.vue
@@ -12,10 +12,83 @@
     },
     created() {},
     mounted() {
-      // this.space()
-      this.phoenix()
+      this.one()
+      // this.phoenix()
     },
     methods: {
+      one() {
+        let container
+        let camera, scene, renderer
+        let mesh = [], mixer = []
+        let controls,bloomComposer
+        let process = 0
+        let prevTime = Date.now()
+        function init() {
+          container = document.getElementById('phoenix')
+          let width = container.offsetWidth <= 414 ? window.innerWidth :  container.offsetWidth
+          let height = container.offsetHeight <= 414 ? window.innerHeight : container.offsetHeight
+          camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000)
+          camera.position.set(0,0,300)
+          scene = new THREE.Scene()
+
+          const light1 = new THREE.DirectionalLight(0xFFFFFF, 1)
+          light1.position.set(1,1,1).normalize()
+          scene.add(light1)
+          const light2 = new THREE.DirectionalLight(0xFFFFFF, 1)
+          light1.position.set(-1,-1,-1).normalize()
+          scene.add(light2)
+
+          const gt = new THREE.TextureLoader().load( "./static/three/grasslight-big.jpg" );
+          // const gt = new THREE.TextureLoader().load( "http://head.xxroom.xyz/lisgsx4cwBF8-6HnXbsP51pob76_" );
+          const gg = new THREE.PlaneGeometry( 192, 108,1,1 );
+          const gm = new THREE.MeshPhongMaterial( { color: 0xffffff, map: gt } );
+          const ground = new THREE.Mesh( gg, gm );
+          ground.rotation.x = - Math.PI / 2;
+          ground.material.map.repeat.set( 4, 2.25 ); //平面贴图重复数
+          ground.material.map.wrapS = ground.material.map.wrapT = THREE.RepeatWrapping;
+          ground.material.map.encoding = THREE.sRGBEncoding;
+          ground.receiveShadow = true;
+          // scene.add( ground );
+
+          const loader = new THREE.GLTFLoader()
+          loader.load('./static/three/gltf/straight.glb', function (gltf) {
+            console.log(gltf)
+            mesh[0] = gltf.scene
+            mesh[1] = clone(gltf.scene)
+            mesh[1].position.z = -110
+            // mesh[0].rotation._x = 0
+            // mesh[0].rotateX(mesh[0].rotation._x)
+            scene.add(mesh[0],mesh[1])
+          })
+          const axesHelper = new THREE.AxesHelper( 50 );
+          scene.add( axesHelper );
+
+          renderer = new THREE.WebGLRenderer( {alpha: true,antialias:true, } );
+          // renderer = new THREE.WebGLRenderer();
+          //您可以将透明颜色保留为默认值。
+          renderer.setClearColor( 0x000,0); //default
+          renderer.setPixelRatio(window.devicePixelRatio) //像素比
+          renderer.setSize(width, height)
+          renderer.outputEncoding = THREE.sRGBEncoding //真彩色，不加的话颜色会与ps中图像看上去的不同
+          container.appendChild(renderer.domElement)
+
+          controls = new THREE.OrbitControls(camera,renderer.domElement);// 初始化控制器
+          controls.target.set(0, 0, 0);// 设置控制器的焦点，使控制器围绕这个焦点进行旋转
+
+          // renderer.autoClear = false
+        }
+        function animate() {
+          requestAnimationFrame(animate)
+          render()
+        }
+        function render() {
+          // camera.lookAt(0, 0, 0)
+
+          renderer.render(scene, camera)
+        }
+        init()
+        animate()
+      },
       phoenix() {
         let container
         let camera, scene, renderer
