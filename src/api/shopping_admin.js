daaf922569caf5d80769e16f4cefe4cc902bfa13
zxm <xiao3017@sina.com>
2022-02-16 18:24:31 +0800

    'goods'

--- src/api/shopping_admin.js
+++ src/api/shopping_admin.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 
+/*新增商品*/
 export function createGoods(data) {
   return request({
     url: '/z/shopping/createGoods',
@@ -7,14 +8,22 @@ export function createGoods(data) {
     data
   })
 }
-
+//更新商品
+export function updateGoods(data) {
+  return request({
+    url: '/z/shopping/updateGoods',
+    method: 'post',
+    data
+  })
+}
+//商品分类列表
 export function categoryList() {
   return request({
     url: '/z/shopping/categoryList',
     method: 'get',
   })
 }
-
+//商品列表
 export function goodsLists(data) {
   return request({
     url: '/z/shopping/goodsLists',
